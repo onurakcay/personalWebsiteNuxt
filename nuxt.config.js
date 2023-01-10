27ce47f6978da27a875acf1e504da58379c6ab63
Onur AKCAY <onurakcay.tr@gmail.com>
2023-01-10 23:48:22 +0300

    title changed

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,8 @@ import colors from "vuetify/es5/util/colors";
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: "%s - personalWebsiteNuxt",
-    title: "personalWebsiteNuxt",
+    titleTemplate: "%s - Fullstack Web Developer",
+    title: "Onur AKCAY",
     htmlAttrs: {
       lang: "en",
     },
